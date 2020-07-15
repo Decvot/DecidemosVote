16493cff0f3133845a80ee89a210934c474751c1
Akrem Hammami <akrem@gmx.us>
2020-07-15 17:38:27 +0200

    modifing bureaux schema

--- voting/models/bureaux.js
+++ voting/models/bureaux.js
@@ -1,24 +1,31 @@
 const mongoose = require('mongoose');
 
 const bureauxSchema = mongoose.Schema({
+    
+    codeBureau:{
+        type:String,
+        required:true
+    },
+    
     nom:{
         type:String,
         required:[true,'nom est oblegatoire']
     },
+
     adresse:{
         type:String,
         required:true
     },
     commune:{
-        type:mongoose.Schema.ObjectId,
+        type:String,
         ref:'Commune'
     },
     canton:{
-        type:mongoose.Schema.ObjectId,
+        type:String,
         ref:'Canton'
     },
     circonscription:{
-        type:mongoose.Schema.ObjectId,
+        type:String,
         ref:'Circonscription'
 
     }
