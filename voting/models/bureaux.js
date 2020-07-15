7c609253bb236d705da507c7aa4b5bbc85647444
Rabbeg Mouhib <rabbegmouhib@gmail.com>
2020-07-15 12:01:25 +0100

    add commune model

--- voting/models/bureaux.js
+++ voting/models/bureaux.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
-const bureauSchema = mongoose.Schema({
+const bureauxSchema = mongoose.Schema({
     nom:{
         type:String,
         required:[true,'nom est oblegatoire']
     },
-    
+    adresse:{
+        type:String,
+        required:true
+    },
+    commune:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Commune'
+    },
+    canton:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Canton'
+    },
+    circonscription:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Circonscription'
+
+    }
 })
+
+const Bureaux = mongoose.model('Bureaux',bureauxSchema);
+module.exports = Bureaux ;
