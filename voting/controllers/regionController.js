386c6031fe46b4dc3984ae9d21810af4ba2c1281
Akrem Hammami <akrem@gmx.us>
2020-07-21 17:57:06 +0200

    regionUpdate

--- voting/controllers/regionController.js
+++ voting/controllers/regionController.js
@@ -1,5 +1,5 @@
 const region = require('../models/region');
-const dep=require('../models/departement');
+
 exports.createRegion = async (req, res) => {
     try {
       const newDoc = await region.create(req.body);
@@ -19,27 +19,7 @@ exports.createRegion = async (req, res) => {
 exports.findAllRegions=async(req,res)=>{
 
     try{
-      const doc=  await region.aggregate([
-        { $lookup:
-           {
-             from: 'Departement',
-             localField: 'codeReg',
-             foreignField: 'codRegion',
-             as: 'orderdetails'
-           }
-         }
-        ])
-      //   .toArray(function(err, res) {
-      //    if (err) throw err;
-      //    console.log(JSON.stringify(res));
-        
-      //  });
-      //  region.find({});
-        
-        // const doc2 = departement.find({});
-        // console.log(doc2);
-      console.log(doc)
-
+        const doc= await region.find({});
         if (!doc) throw 'no document found';
 
 
