ab0faaa2ca27175dcae886ac82854026774a5706
Akrem Hammami <akrem@gmx.us>
2020-07-24 12:08:41 +0200

    updateCantonAPI

--- voting/controllers/cantonController.js
+++ voting/controllers/cantonController.js
@@ -1,7 +1,10 @@
 const canton = require('../models/cantons');
-const dep = require('../models/departement');
-const fs = require('fs')
 
+
+
+/*CRUD API of canton*/
+
+//create a new 'canton' 
 exports.createCanton = async (req, res) => {
     try {
       const newDoc = await canton.create(req.body);
@@ -14,16 +17,19 @@ exports.createCanton = async (req, res) => {
       });
     } catch (err) {
       res.status(400).json({
-        status: 'fail',
+        status: 'error at canton create',
         message: err,
       });
     }
   };
+
+
+
+  //get a  specific 'canton' using 'codeCanton' 
 exports.getOneCanton = async (req, res) => {
 
     try {
-
-      const doc = await canton.findOne({codeCanton:req.params.code})
+      const doc = await canton.findOne({codeCanton:req.params.codeCanton})
        
       if (!doc) throw 'no document found';
   
@@ -34,132 +40,144 @@ exports.getOneCanton = async (req, res) => {
         },
       });
     } catch (err) {
-      res.status(400).json({
+      res.status(404).json({
         status: 'fail',
         message: err,
       });
     }
   };
 
+
+
+
+  //get all 'canton' 
   exports.getAllCanton = async (req, res) => {
     try {
-      const doc = await canton.find({})
-      res.status(200).json({
-        status: 'success',
-        data: {
-          doc,
-        },
-      });
-    } catch (err) {
-      res.status(400).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+      const docs = await canton.find({});
+      if (!docs) throw 'no documents found';
 
-  exports.updateOneCanton = async (req, res) => {
-    try {
-      const updatedDoc = await canton.findOneAndUpdate({codeCanton:req.params.code}, req.body, {
-        new: true,
-        //to run the validator again
-        runValidators: true,
-      });
-      if (!updatedDoc) throw 'document not found  ss';
-      res.status(201).json({
-        status: 'success',
-        data: {
-          updatedDoc,
-        },
-      });
-    } catch (err) {
-      res.status(400).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-}
-
-exports.deleteOneCanton =  async (req, res) => {
-    try {
-      const doc = await canton.findOneAndDelete({codeCanton:req.params.code});
-      if (!doc) {
-        throw 'no document found with this id';
-      }
-  
-      res.status(204).json({
-        status: 'success',
-        message: 'doc deleted !',
-      });
-    } catch (err) {
-      res.status(400).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
-
-   exports.cantonDep = async (req,res)=>{
-    try {
-      const data = await canton.aggregate([
-        {
-          $group:{
-          _id:'$codeDepartement',
-          bureaux:{ $push: '$bureaux' },
-          
-         
+        res.status(200).json({
+            status: 'success',
+            data: {
+              docs,
+            },
+          });
+        } catch (err) {
+          res.status(404).json({
+            status: 'fail',
+            message: err,
+          });
         }
-       },
-        { $sort : { _id : 1 } },
-      
-      ])
-      
-      data.forEach(async depe => {
-        let bureaux = []
-        if(depe._id < "10"){
-          depe._id =depe._id.replace('0','')
-         /*  depe._id = "0"+depe._id */
-        }
-        
+      };
+
+
+
+      //update a 'canton'
+      exports.updateOneCanton = async (req, res) => {
         try {
-          depe.bureaux.forEach(async elt => {
-            
-            if(elt.length){
-              bureaux = bureaux.concat(elt)
-            }
-           
-            //await dep.findOneAndUpdate({codeDepartement:depe._id},doc)
-          })  
-          if(+depe._id === 13){
+          const updatedDoc = await canton.findOneAndUpdate({codeCanton:req.params.codeCanton}, req.body, {
+            new: true,
+            //to run the validator again
+            runValidators: true,
+          });
+          if (!updatedDoc) throw 'document not found  ss';
+          res.status(201).json({
+            status: 'success',
+            data: {
+              updatedDoc,
+            },
+          });
+        } catch (err) {
+          res.status(400).json({
+            status: 'fail',
+            message: err,
+          });
+        }
+    }
 
-           console.log('here');
+
+
+    //delete a specific 'canton'
+    exports.deleteOneCanton =  async (req, res) => {
+        try {
+          const doc = await canton.findOneAndDelete({codeCanton:req.params.codeCanton});
+          if (!doc) {
+            throw 'no document found with this id';
           }
-          await dep.findOneAndUpdate({codeDepartement:+depe._id},{bureaux:bureaux})
-
-          //AUBE
+      
+          res.status(204).json({
+            status: 'success',
+            message: 'doc deleted !',
+          });
+        } catch (err) {
+          res.status(404).json({
+            status: 'fail',
+            message: err,
+          });
+        }
+      };
+    
+      //this code is used for adding bureaux in canton, as soon as 'bureaux'added correctly this code will be deleted  
+      //const dep = require('../models/departement');
+      //exports.cantonDep = async (req,res)=>{
+      //   try {
+      //     const data = await canton.aggregate([
+      //       {
+      //         $group:{
+      //         _id:'$codeDepartement',
+      //         bureaux:{ $push: '$bureaux' },
+              
+             
+      //       }
+      //      },
+      //       { $sort : { _id : 1 } },
           
-          //console.log('bon') 
+      //     ])
           
-        } catch (error) {
-          
-       }
-        
-       
-      })
-     
-       res.status(201).json({
-         status: 'success',
-         data: {
-           data
-         },
-       });
-     } catch (err) {
-       res.status(400).json({
-         status: 'fail',
-         message: err,
-       });
-     }
-   }; 
-
-
-  
+      //     data.forEach(async depe => {
+      //       let bureaux = []
+      //       if(depe._id < "10"){
+      //         depe._id =depe._id.replace('0','')
+      //        /*  depe._id = "0"+depe._id */
+      //       }
+            
+      //       try {
+      //         depe.bureaux.forEach(async elt => {
+                
+      //           if(elt.length){
+      //             bureaux = bureaux.concat(elt)
+      //           }
+               
+      //           //await dep.findOneAndUpdate({codeDepartement:depe._id},doc)
+      //         })  
+      //         if(+depe._id === 13){
+    
+      //          console.log('here');
+      //         }
+      //         await dep.findOneAndUpdate({codeDepartement:+depe._id},{bureaux:bureaux})
+    
+      //         //AUBE
+              
+      //         //console.log('bon') 
+              
+      //       } catch (error) {
+              
+      //      }
+            
+           
+      //     })
+         
+      //      res.status(201).json({
+      //        status: 'success',
+      //        data: {
+      //          data
+      //        },
+      //      });
+      //    } catch (err) {
+      //      res.status(400).json({
+      //        status: 'fail',
+      //        message: err,
+      //      });
+      //    }
+      //  }; 
+    
--- voting/models/cantons.js
+++ voting/models/cantons.js
@@ -3,19 +3,25 @@ const Bureaux = require('./bureaux');
 
 
 const cantonSchema=mongoose.Schema({
+    //the code of 'canton'
     codeCanton:{
         type:String,
         required:true,
         unique:true
-    }, codeDepartement:{
+    },
+    
+    //the code of 'departement' that include that specific 'canton'
+    codeDepartement:{
         type:String,
         required:true,
     },
+    //the name of the 'canton'
     libelle:{
         type:String,
         required:true
 
     },
+    //the list of 'bureaux' that are included in the 'catnon'
     bureaux:{
         type:[]
     }
