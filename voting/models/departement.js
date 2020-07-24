45452ba09492ee10688c2c384c4b79aafe050cb7
Akrem Hammami <akrem@gmx.us>
2020-07-24 13:10:14 +0200

    updateDepartementAPI

--- voting/models/departement.js
+++ voting/models/departement.js
@@ -2,32 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const departementSchema=new Schema({
+        //the code of 'departement'
+
     codeDepartement:{
 
         type:String,
         required:true,
         unique:true
     },
+        //the code of 'region' that include that specific 'departement'
+
     codRegion:{
         type:String,
         required:true,
     },
+        //the name of the 'departement'
+
     libelle:{
         type:String,
         required:true
 
     },
-    bureaux:{type:[]}
+    
+    //the list of 'bureaux' that are included in the 'departement'
+
+    bureaux:{
+        type:[]
+    }
     
 
-})
+});
 
 
-departementSchema.virtual('testBureaux',{
+/* departementSchema.virtual('testBureaux',{
     ref:'Canton',
    foreignField: 'codeDepartement',
     localField: 'codeDepartement',
-}) 
+})  */
  
  var Departement=mongoose.model('Departement',departementSchema);
 module.exports=Departement;
--- voting/routes/departement.js
+++ voting/routes/departement.js
@@ -2,16 +2,17 @@ var express = require('express');
 const departementController = require('../controllers/departementController')
 var router = express.Router();
 
-/* GET bureaux . */
+
 
 router.route('/')
 .get(departementController.getAllDepartement)
-.post(departementController.createDepartement)
+.post(departementController.createDepartement);
+
 
 router.route('/:codeDepartement')
 .get(departementController.getOneDepartement)
 .patch(departementController.updateOneDepartement)
-.delete(departementController.deleteOneDepartement)
+.delete(departementController.deleteOneDepartement);
 
 
 module.exports = router;
